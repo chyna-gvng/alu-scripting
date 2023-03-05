3c16939abd83310b6f493d5590e6463e8fdffff4
Walter <w.angoye@alustudent.com>
2023-03-05 11:49:22 +0300

    api_advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,6 +12,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         return response.json()["data"]["subscribers"]
     else:

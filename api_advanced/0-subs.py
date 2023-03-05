97bf561e1e4deeca42bc145b332d67dc281c7998
Walter <w.angoye@alustudent.com>
2023-03-05 11:42:33 +0300

    api_advanced

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    funtion that queries Reddit API
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+        return the total number of subscribers for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
+        return 0

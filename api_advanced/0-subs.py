5a9483d1e2bc415e70c09c0e9c563e90ec49b2ea
Walter <w.angoye@alustudent.com>
2023-03-05 12:06:46 +0300

    api_advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ number_of_subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
 

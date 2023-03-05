68ddf7df025e9b81bd1bafd3c5eee29959696796
Walter <w.angoye@alustudent.com>
2023-03-05 13:50:50 +0300

    api_advanced

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
-                           headers = {'User-Agent': 'Mozilla/5.0'})
+                           headers={'User-Agent': 'Mozilla/5.0'})
 
     if request.status_code == 200:
         data = request.json()

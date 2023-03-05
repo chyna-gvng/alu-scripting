5ef7c2f7c67a853310d9737d1b9d19125bbd2726
Walter <w.angoye@alustudent.com>
2023-03-05 13:29:49 +0300

    api_advanced

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -4,34 +4,30 @@ import requests
 
 
 def count_words(subreddit, word_list, after=None, count_dict=None):
-    """ print a sorted count of given keywords """
+    """ prints a sorted count of given keywords """
     if count_dict is None:
         count_dict = {}
     headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/.json?limit=100'
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
     if after:
-        url += f'&after={after}'
+        url += '&after={}'.format(after)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print('Invalid subreddit')
+    if response.status_code != 200:
         return
     data = response.json()
     children = data['data']['children']
     for child in children:
         title = child['data']['title'].lower()
         for word in word_list:
+            if ' ' in word or '.' in word or '!' in word or '_' in word:
+                continue
             if word.lower() in title:
                 count_dict[word.lower()] = count_dict.get(word.lower(), 0)
                 count_dict[word.lower()] += title.count(word.lower())
     after = data['data']['after']
     if after is None:
-        if len(count_dict) == 0:
-            print('No matching words found')
-            return
-        count_dict = sorted(count_dict.items(), key=lambda kv: (-kv[1], kv[0]))
-        for k, v in count_dict:
-            print(f'{k}: {v}')
+        for word, count in sorted(count_dict.items(),
+                                  key=lambda x: (-x[1], x[0])):
+            print('{}: {}'.format(word, count))
     else:
         count_words(subreddit, word_list, after, count_dict)
